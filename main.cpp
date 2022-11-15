d27ea478aa5dc40605295d8b734f0815dbf73e1e
Jacob-32587 <jgbs32587@gmail.com>
2022-11-14 20:54:07 -0500

    Added more safety for user inputs
    The only inputs that will be accepted are those accepted
    by strtold()

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <math.h>
+#include <ctype.h>
 
-#define QFC_ERROR_MSG "Unexpected Input, try again\n"
+#define QFC_ERROR_MSG "Formatting error on last input, try again\n"
 #define QFC_BUFF_SIZE 32
 
 long double promptAndGet(const char *message, char *buff);
+bool onlyZeroF(const char *input);
 
 int main()
 {
@@ -38,21 +40,56 @@ int main()
 		}
 
 		break;
-	}
-
-	// printf("%f %f %f", (double)a, (double)b, (double)c);
+	} // User input collection loop
 
 	return 0;
 }
 
+// ARG0: Message that the user will be prompted to enter data with
+// ARG1: Buffer to hold data
+
 long double promptAndGet(const char *message, char *buff)
 {
+	long double retVal = 0.0L;
+
 	printf("%s", message);
 
-	if (fgets(buff, QFC_BUFF_SIZE, stdin) != NULL)
+	if (fgets(buff, QFC_BUFF_SIZE, stdin) != nullptr)
 	{
-		return strtold(buff, nullptr);
+		retVal = strtold(buff, nullptr);
+
+		printf("%d", onlyZeroF(buff));
+
+		if (retVal == 0.0 && !onlyZeroF(buff))
+			return INFINITY;
+		else
+			return retVal;
 	}
 	else
 		return INFINITY;
+}
+
+// ARG0: Checks in the null-terminated character string is of zero value
+// ie: "0.0000000" "0.0" "0"
+bool onlyZeroF(const char *input)
+{
+	short decimalCnt = 0; // Track number of decimal places
+
+	while (*input != '\0')
+	{
+		// Only a valid character if 1 decimal appears and any number of zero's
+		if (*input == '0' || isspace(*input))
+			;
+		else if (*input == '.')
+			decimalCnt++;
+		else
+			return false;
+
+		if (decimalCnt == 2) // Cannot have more than 1 decimal marker
+			return false;
+
+		input++; // Next character
+	}
+
+	return true; // Past all tests
 }
